7d1f437417cfeabb8a357211fb66d1f2e0932c5d
Marko Tadic <marko.tadic005@gmail.com>
2021-03-20 18:16:28 +0100

    add buttons for all radio stations

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
 import * as React from "react";
+import radioStations from "./components/radioStations";
 
 export interface IAppProps {}
 
+interface RadioStation {
+  name: string;
+  src: string;
+}
+
 export default function App(props: IAppProps) {
   const [isPlaying, setPlaying] = React.useState(false);
+  const [station, setStation] = React.useState<RadioStation | null>(null);
   const audioRef = React.useRef<HTMLAudioElement>(null);
 
   return (
     <div>
-      <audio ref={audioRef} src="https://streaming.radiojat.rs/radiojat.mp3" />
-      <button
-        onClick={() => {
-          if (isPlaying) {
-            audioRef.current?.pause();
-          } else {
-            audioRef.current?.play();
-          }
-          setPlaying(!isPlaying);
-        }}
-      >
-        {isPlaying ? "Pause" : "Play"}
-      </button>
+      {station != null && (
+        <div>
+          <audio ref={audioRef} src={station.src} />
+          <button
+            onClick={() => {
+              if (isPlaying) {
+                audioRef.current?.pause();
+              } else {
+                audioRef.current?.play();
+              }
+              setPlaying(!isPlaying);
+            }}
+          >
+            {isPlaying ? "Pause" : "Play"}
+          </button>
+        </div>
+      )}
+
+      {radioStations.map((station) => {
+        return (
+          <button
+            onClick={() => {
+              setStation(station);
+            }}
+          >
+            {station.name}
+          </button>
+        );
+      })}
     </div>
   );
 }
---
+++ src/components/radioStations.js
@@ -0,0 +1,9 @@
+const radios = [
+  { name: "otvoreni", src: "http://stream.otvoreni.hr/otvoreni" },
+  { name: "radio JAT", src: "https://streaming.radiojat.rs/radiojat.mp3" },
+  { name: "enterZG", src: "http://live.enterzagreb.hr:8023/;" },
+  { name: "goldFM", src: "http://live.goldfm.hr:8068/;" },
+  { name: "antenaZG", src: "http://live.antenazagreb.hr:8000/;" },
+];
+
+export default radios;
